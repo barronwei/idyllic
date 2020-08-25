a6871bb2a1955319d3cd1fc84ecf15b7a35b4bb8
Barron Wei <barronwei@college.harvard.edu>
2020-08-25 18:21:09 -0400

    fix useLocal

--- src/app/utils/useLocal.js
+++ src/app/utils/useLocal.js
@@ -1,24 +1,21 @@
 import { useEffect, useMemo, useReducer, useState } from "react"
-import { encode, decode } from "../utils/encoding"
 
 function useLocalReducer(id, init, reducer) {
-  const temp = useMemo(() => decode(localStorage.getItem(id)), [id])
-  const data = useMemo(() => temp ? JSON.parse(temp) : temp, [temp])
+  const data = useMemo(() => JSON.parse(localStorage.getItem(id)), [id])
   const r = useReducer(reducer, data ?? init)
   const [[state, dispatch]] = [r]
   useEffect(() => {
-    localStorage.setItem(id, encode(JSON.stringify(state)))
+    localStorage.setItem(id, JSON.stringify(state))
   }, [id, state])
   return [state, dispatch]
 }
 
 function useLocalStorage(id, init) {
-  const temp = useMemo(() => decode(localStorage.getItem(id)), [id])
-  const data = useMemo(() => (temp ? JSON.parse(temp) : temp), [temp])
+  const data = useMemo(() => JSON.parse(localStorage.getItem(id)), [id])
   const s = useState(data ?? init)
   const [[state, setState]] = [s]
   useEffect(() => {
-    localStorage.setItem(id, encode(JSON.stringify(state)))
+    localStorage.setItem(id, JSON.stringify(state))
   }, [id, state])
   return [state, setState]
 }
