fc3509603a8d9db484320ad023ee69de8c23b6f0
Barron Wei <barronwei@college.harvard.edu>
2020-08-25 18:21:22 -0400

    combine text code

--- src/app/reduction/reducer.js
+++ src/app/reduction/reducer.js
@@ -8,24 +8,28 @@ function reducer(state, action) {
       break
     }
 
-    case "ADD_TEXT": {
-      const { files, texts } = state
+    case "ADD_LOAD": {
+      const { files, loads } = state
       const { names } = payload
       const lambda = ({ path }) => path === names
-      const opened = texts.filter(lambda)
+      const opened = loads.filter(lambda)
       const exists = files.filter(lambda)
-      if (opened.length === 0 || !exists) break
-      const trans = [...texts, ...exists]
-      res = { ...state, texts: trans }
+      if (opened.length !== 0 || !exists) break
+      const trans = [...loads, ...exists]
+      res = { ...state, loads: trans }
       break
     }
 
-    case "REM_TEXT": {
-      const { texts } = state
+    case "REM_LOAD": {
+      const { loads } = state
       const { names } = payload
       const lambda = ({ name }) => name !== names
-      res = { ...state, texts: texts.filter(lambda) }
+      res = { ...state, loads: loads.filter(lambda) }
       break
+    }
+
+    default: {
+      throw Error("Invalid Dispatch!")
     }
   }
 
