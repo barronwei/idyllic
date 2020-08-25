46861cd13e03331ce84ba0b9276ae78efc21b5ed
Barron Wei <barronwei@college.harvard.edu>
2020-08-25 18:22:48 -0400

    use Tabs

--- src/app/components/panes/Code.js
+++ src/app/components/panes/Code.js
@@ -1,7 +1,18 @@
 import React from "react"
+import AceEditor from "react-ace"
+import { decode } from "../../utils/encoding"
 
-function Code() {
-  return <div>Empty</div>
+function Code({ name, data }) {
+  return (
+    <AceEditor
+      mode="java"
+      name={name}
+      defaultValue={decode(data)}
+      editorProps={{ $blockScrolling: true }}
+      width={"1000px"}
+      height={"500px"}
+    />
+  )
 }
 
 export { Code }
